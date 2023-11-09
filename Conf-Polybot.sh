9d9c9700140c8c50ce4541e564b703fb2d1fa758
Amiran Ivgi <109898333+AmiranIV@users.noreply.github.com>
2023-11-09 13:57:29 +0200

    Create Conf-Polybot.sh

---
+++ Conf-Polybot.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Update package lists
+sudo apt update
+
+# Install Docker and answer 'Y' when prompted
+sudo apt install -y docker.io
+
+# Run the Docker container with the 'always' restart policy
+sudo docker run -p 8443:8443 --restart always amiraniv/aws-polybot:v1.0
